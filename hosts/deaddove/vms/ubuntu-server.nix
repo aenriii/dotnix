b0428ad7ac05f7a22b3ad0bd286598f7bc9c8f57
Aenri Lovehart <aenri@loveh.art>
2026-09-16 03:01:49 -0400

    fix(deaddove): fixed percieved ordering for vms

--- hosts/deaddove/vms/ubuntu-server.nix
+++ hosts/deaddove/vms/ubuntu-server.nix
@@ -2,12 +2,12 @@
 let
   vmUuid = "85b8315b-0916-4e0f-b659-51fbb6f93079";
 in {
-  definition = libvirt.domain.writeXML libvirt.domain.templates.linux {
+  definition = libvirt.domain.writeXML (libvirt.domain.templates.linux {
     name = "ubuntu-server";
     uuid = vmUuid;
     memory = { count = 4; unit = "GiB"; };
     storage_vol = { pool = "default"; volume = "${vmUuid}.qcow2"; };
     install_vol = "/home/aenri/VMs/ISOs/ubuntu-server-lts-26.04.1.iso";
     nvram_path = "/home/aenri/VMs/nvram/${vmUuid}.fd";
-  };
+  });
 }
